443d14912b2315ee4d1f754a445cc98f745ee5db
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2018-05-31 12:48:50 +0200

    finalize data_extend_zone.f90

--- TOOLS/data_extend_zone.f90
+++ TOOLS/data_extend_zone.f90
@@ -21,8 +21,8 @@ PROGRAM data_extend_zone
   !!----------------------------------------------------------------------
   IMPLICIT NONE
   INTEGER :: ji,jj
-  INTEGER :: npiglo, npjglo, nx, ny, nrim
-  INTEGER :: ncid, id, ierr, ncidm
+  INTEGER :: npiglo, npjglo, nx, ny, nrim, nxx, nyy
+  INTEGER :: ncid, id, ierr, ncidd, idbat
   INTEGER :: narg, ijarg
   INTEGER :: iimin, iimax, ijmin, ijmax
   INTEGER, DIMENSION(:,:), ALLOCATABLE :: mask        ! on eDomain, inner part on Domain
@@ -32,6 +32,7 @@ PROGRAM data_extend_zone
 
   CHARACTER(LEN=80) :: cf_bathy='eNATL60_BATHY_GEBCO_2014_2D_raw_v2.nc'
   CHARACTER(LEN=80) :: cf_data
+  CHARACTER(LEN=80) :: cf_out='none'
   CHARACTER(LEN=80) :: cv_data
   CHARACTER(LEN=80) :: cldum
   !!------------------------------------------------------------------------
@@ -42,7 +43,7 @@ PROGRAM data_extend_zone
 
   IF ( narg == 0 ) THEN
      PRINT *,' usage :  data_extend_zone -b BATHY-new -d DATA-old -v VAR-old ...'
-     PRINT *,'          [-w imin imax jmin jmax ]'
+     PRINT *,'          [-o FILE-out] [-w imin imax jmin jmax ]'
      PRINT *,'      '
      PRINT *,'     PURPOSE :'
      PRINT *,'        Create a file of the size of the new bathymetry, with old values copied'
@@ -56,11 +57,12 @@ PROGRAM data_extend_zone
      PRINT *,'     OPTIONS :'
      PRINT *,'       -w imin imax jmin jmax : specify the position of old grid with respect ' 
      PRINT *,'          to the new one. Default is for NATL60 into eNATL60'
+     PRINT *,'       -o FILE-out: specify name of output file, instead of <BATHY-new>.wrk'
      PRINT *,'      '
      PRINT *,'      '
      PRINT *,'     OUTPUT : '
      PRINT *,'       netcdf file : ', TRIM(cf_out) 
-     PRINT *,'         variables : ', TRIM(cv_out),' (    )'
+     PRINT *,'         variables : Bathymetry !! '
      PRINT *,'      '
      PRINT *,'      '
      STOP
@@ -73,11 +75,12 @@ PROGRAM data_extend_zone
      CASE ( '-b'   ) ; CALL getarg(ijarg, cf_bathy ) ; ijarg=ijarg+1
      CASE ( '-d'   ) ; CALL getarg(ijarg, cf_data  ) ; ijarg=ijarg+1
      CASE ( '-v'   ) ; CALL getarg(ijarg, cv_data  ) ; ijarg=ijarg+1
-     ! options
+        ! options
      CASE ( '-w'   ) ; CALL getarg(ijarg, cldum  )   ; ijarg=ijarg+1 ; READ(cldum,*) iimin
         ;            ; CALL getarg(ijarg, cldum  )   ; ijarg=ijarg+1 ; READ(cldum,*) iimax
         ;            ; CALL getarg(ijarg, cldum  )   ; ijarg=ijarg+1 ; READ(cldum,*) ijmin
-        ;            ; CALL getarg(ijarg, cldum  )   ; ijarg=ijarg+1 ; READ(cldum,*) ijmmax
+        ;            ; CALL getarg(ijarg, cldum  )   ; ijarg=ijarg+1 ; READ(cldum,*) ijmax
+     CASE ( '-o'   ) ; CALL getarg(ijarg, cf_out )   
      CASE DEFAULT    ; PRINT *, ' ERROR : ', TRIM(cldum),' : unknown option.'; STOP 1
      END SELECT
   ENDDO
@@ -94,12 +97,18 @@ PROGRAM data_extend_zone
   nx=iimax - iimin +1
   ny=ijmax - ijmin +1
 
-  CALL system ( "cp "//TRIM(cf_bathy)//" "//TRIM(cf_bathy)//".wrk" )
-  cf_bathy=TRIM(cf_bathy)//".wrk"
+  IF ( TRIM(cf_out) == 'none' ) THEN
+     cf_out=TRIM(cf_bathy)//".wrk"
+  ENDIF
+
+  CALL system ( "cp "//TRIM(cf_bathy)//" "//TRIM(cf_out))
+  ! work on cf_out directly
+  cf_bathy=cf_out
+  PRINT *,' WORKING ON ',TRIM(cf_bathy)
 
   ! Read size of edomain :
   PRINT *,' OPENING '//TRIM(cf_bathy)
-  ierr=NF90_OPEN(cf_bathy, NF90_WRITE, ncid)
+  ierr= NF90_OPEN(cf_bathy, NF90_WRITE, ncid)
   ierr= NF90_INQ_DIMID(ncid,'x',id) ; ierr=NF90_INQUIRE_DIMENSION(ncid, id, len=npiglo )
   ierr= NF90_INQ_DIMID(ncid,'y',id) ; ierr=NF90_INQUIRE_DIMENSION(ncid, id, len=npjglo )
 
@@ -110,34 +119,28 @@ PROGRAM data_extend_zone
 
   ! allocate bathy and mask array
   ALLOCATE ( bathy(npiglo, npjglo), mask(npiglo, npjglo) )
+  ierr = NF90_INQ_VARID(ncid,'Bathymetry',idbat)
+  ierr = NF90_GET_VAR(ncid,idbat,bathy)
 
   ! read Domain mask
-  mask=2
-  ierr=NF90_OPEN(cf_mask,NF90_NOWRITE, ncidm)
-  ierr=NF90_INQ_VARID(ncidm,'tmask',id)
-  ierr=NF90_GET_VAR(ncidm, id, mask(iimin:iimax,ijmin:ijmax), start=(/1,1,1,1/),count=(/nx,ny,1,1/) )
-  ierr=NF90_CLOSE(ncidm)
+  ! build the  mask
+  mask=1
+  WHERE ( bathy /= 0. ) bathy=1.
+  ! Open and read data file
+  ierr = NF90_OPEN(cf_data,NF90_NOWRITE,ncidd)
+  ierr = NF90_INQ_DIMID(ncidd,'x',id) ; ierr=NF90_INQUIRE_DIMENSION(ncidd, id, len=nxx )
+  ierr = NF90_INQ_DIMID(ncidd,'y',id) ; ierr=NF90_INQUIRE_DIMENSION(ncidd, id, len=nyy )
 
-  ! modify mask : put 2 where we do not want to change the raw bathy 
-  !  (1) on the rim ( 3*15 +1 points SOUTH, EAST and North --no WEST -- )
-  nrim=3*15+1
-  mask(icorner(1):icorner(2)        , jcorner(1):jcorner(1)+nrim   ) = 2 
-  mask(icorner(2):icorner(2)-nrim:-1, jcorner(2):jcorner(3)        ) = 2
-  mask(icorner(4):icorner(2)        , jcorner(4):jcorner(4)-nrim:-1) = 2 
-  ! (2) Do not mask hudson bay
-  mask(1:1855,jcorner(4)-1432:jcorner(4)) = 2 
-
-  ! read eBathy :
-  ierr = NF90_INQ_VARID(ncid,'Bathymetry',id)
-  ierr = NF90_GET_VAR(ncid,id,bathy)
-
-  ! mask bathy
-  WHERE ( mask == 0 ) bathy=0.
-  ! put minimum depth ( 3m) where mask is 1 and bathy=0
-  WHERE ( bathy == 0. .AND. mask == 1 ) bathy=rdepmin
+  IF ( nxx /= nx .OR. nyy /= ny ) THEN
+     PRINT *, ' ERROR : mismatch in specified window size, incoherent with data file.'
+     STOP
+  ENDIF
+  ierr = NF90_INQ_VARID(ncidd,cv_data,id)
+  ierr = NF90_GET_VAR(ncidd,id,bathy(iimin:iimax,ijmin:ijmax) )
+ 
 
   ! write back modifies bathy to file
-  ierr=NF90_PUT_VAR(ncid,id,bathy)
+  ierr=NF90_PUT_VAR(ncid,idbat,bathy)
   ierr=NF90_CLOSE(ncid)
 
 END PROGRAM data_extend_zone
